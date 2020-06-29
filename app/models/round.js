34c9fd631999ab87a75b9c1100b6caf924e21c5d
Andrew-Chun <i.am.andrewchun@gmail.com>
2020-06-29 09:24:13 -0500

    CRUD rounds with user as a reference

--- app/models/round.js
+++ app/models/round.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 
-const exampleSchema = new mongoose.Schema({
-  title: {
+const roundSchema = new mongoose.Schema({
+  course: {
     type: String,
     required: true
   },
-  text: {
-    type: String,
+  date: {
+    type: Date,
+    required: true
+  },
+  score: {
+    type: Number,
     required: true
   },
   owner: {
@@ -18,4 +22,4 @@ const exampleSchema = new mongoose.Schema({
   timestamps: true
 })
 
-module.exports = mongoose.model('Example', exampleSchema)
+module.exports = mongoose.model('Round', roundSchema)
